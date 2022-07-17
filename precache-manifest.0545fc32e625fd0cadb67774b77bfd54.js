54422cb23f93b9b92683eb23e0621ed37f7e04d7
guigui-guigs <guillaume.rothschmidt@gmail.com>
2022-07-17 19:02:47 +0200

    Updates

---
+++ precache-manifest.0545fc32e625fd0cadb67774b77bfd54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "33ac1f2255b1ea55b925",
+    "url": "/formation/static/css/main.9480bfc5.chunk.css"
+  },
+  {
+    "revision": "33ac1f2255b1ea55b925",
+    "url": "/formation/static/js/main.33ac1f22.chunk.js"
+  },
+  {
+    "revision": "7ea1d09db556f8ac22b1",
+    "url": "/formation/static/css/1.0d5f3ebf.chunk.css"
+  },
+  {
+    "revision": "7ea1d09db556f8ac22b1",
+    "url": "/formation/static/js/1.7ea1d09d.chunk.js"
+  },
+  {
+    "revision": "d12d9c5932b08362de35",
+    "url": "/formation/static/js/runtime~main.d12d9c59.js"
+  },
+  {
+    "revision": "956acf4f2656d6e8153a05a3acb0e0ff",
+    "url": "/formation/index.html"
+  }
+];
